6df021948ec21cfe6db7bb3a5354948a9886bda4
yBATTE <lucasbattelini1@gmail.com>
2026-05-13 11:34:48 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:5173',
       'https://lb-business.vercel.app',
-      'https://maite-puce.vercel.app/',
+      'https://maite-puce.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
