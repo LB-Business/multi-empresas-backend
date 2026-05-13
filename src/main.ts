1c5686e1bc294550bcdd8d5a07124f05cf5fb6d7
yBATTE <lucasbattelini1@gmail.com>
2026-05-13 11:31:12 -0300

    add maite

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:5173',
       'https://lb-business.vercel.app',
+      'https://vercel.com/ybattes-projects/maite',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
